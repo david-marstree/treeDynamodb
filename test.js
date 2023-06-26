448d4a63faf26fc085cb007b765f8f0a1053f844
David Chu Kuong Io <david@marstree.ltd>
2023-06-26 20:47:18 +0800

    david: add test json file

--- test.js
+++ test.js
@@ -8,18 +8,11 @@ const client = treeDynamodb.createClient({
 const { data } = treeApiRequest.option(require("./test.json"));
 
 const func = async () => {
-  const projectId = Buffer.from(
-    new Date().getTime() + "-" + Math.round(Math.random() * 100) / 100
-  ).toString("base64");
-  const createdAt = parseInt(new Date().getTime() / 1000);
-
-  const response = await treeDynamodb.add({
+  const response = await treeDynamodb.get({
     client,
     table: "apiflowProject-dev",
     data: {
       ...data,
-      projectId,
-      createdAt,
     },
   });
   console.log(response);
